7d10081d0dfd25917dcbd040b43b808a89883a1f
Rishesh Shukla <132539518+realrishesh@users.noreply.github.com>
2025-06-18 21:03:58 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -724,4 +724,4 @@ async def health_check():
         )
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=5000, reload=True) 
+    app.run("app:app", host="0.0.0.0", port=8080, reload=True) 
